16f524cb0a2b8d9f1bff1e8c9a4f0ab68d304258
lliu88 <liu@sfu.ca>
2016-06-18 17:00:08 -0700

    Update generate.sh
    build_json script location changed since perfSOANR 3.5 release.

--- generate.sh
+++ generate.sh
@@ -5,6 +5,6 @@ for file in $(find ./ -name '*.conf' | cut -c 3- | sort)
 do
     # Append the .json extension instead of .config
     output=${file%%.*}.json
-    /opt/perfsonar_ps/mesh_config/bin/build_json -input $file -output $output
+    /usr/lib/perfsonar/bin/build_json -input $file -output $output
     [ $? -ne 0 ] && echo Failure on file $file
 done
